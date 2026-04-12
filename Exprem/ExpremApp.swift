14d33f18882830c019068c8c020df382a3970e1b
Jonathan Limanza <jonathanlimanza@gmail.com>
2026-04-12 20:55:10 +0700

    Initial Commit

---
+++ Exprem/ExpremApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpremApp.swift
+//  Exprem
+//
+//  Created by Jon on 12/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpremApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
