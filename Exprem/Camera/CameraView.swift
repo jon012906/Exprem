e1748c576eb09ce5fd9510ffe78efa717070e11e
Jonathan Limanza <jonathanlimanza@gmail.com>
2026-04-24 09:06:19 +0700

    command the benchmark file

--- Exprem/Camera/CameraView.swift
+++ Exprem/Camera/CameraView.swift
@@ -1,71 +1,71 @@
+////    THIS ONLY FOR BENCHMARKING
+////  CameraView.swift
+////  Exprem
+////
+////  Created by Jon on 22/04/26.
+////
 //
-//  CameraView.swift
-//  Exprem
+//import SwiftUI
+//import AVFoundation
+//import UIKit
+//import Observation
 //
-//  Created by Jon on 22/04/26.
+//struct CameraView: View {
+//    @State private var viewModel = CameraViewModel()
 //
-
-import SwiftUI
-import AVFoundation
-import UIKit
-import Observation
-
-struct CameraView: View {
-    @State private var viewModel = CameraViewModel()
-
-    var body: some View {
-        ZStack {
-            CameraPreview(session: viewModel.getSession())
-                .ignoresSafeArea()
-
-            VStack {
-                Spacer()
-
-                Button(action: {
-                    viewModel.capture()
-                }) {
-                    Circle()
-                        .fill(Color.white)
-                        .frame(width: 70, height: 70)
-                        .overlay(Circle().stroke(Color.black, lineWidth: 2))
-                }
-                .padding(.bottom, 30)
-            }
-        }
-        .onAppear {
-            viewModel.setup()
-        }
-        .onDisappear {
-            viewModel.stop()
-        }
-        .fullScreenCover(isPresented: $viewModel.isCaptured) {
-            if let image = viewModel.image {
-                CapturedImageView(viewModel: viewModel, image: image)
-            }
-        }
-    }
-}
-
-struct CapturedImageView: View {
-    @Bindable var viewModel: CameraViewModel
-    let image: UIImage
-    @Environment(\.dismiss) private var dismiss
-    
-    var body: some View {
-        VStack {
-            Image(uiImage: image)
-                .resizable()
-                .scaledToFit()
-                .ignoresSafeArea()
-                
-            Button("Use Photo"){
-                viewModel.retake()
-                dismiss()
-            }
-            .padding()
-        }
-    }
-    
-
-
-}
+//    var body: some View {
+//        ZStack {
+//            CameraPreview(session: viewModel.getSession())
+//                .ignoresSafeArea()
+//
+//            VStack {
+//                Spacer()
+//
+//                Button(action: {
+//                    viewModel.capture()
+//                }) {
+//                    Circle()
+//                        .fill(Color.white)
+//                        .frame(width: 70, height: 70)
+//                        .overlay(Circle().stroke(Color.black, lineWidth: 2))
+//                }
+//                .padding(.bottom, 30)
+//            }
+//        }
+//        .onAppear {
+//            viewModel.setup()
+//        }
+//        .onDisappear {
+//            viewModel.stop()
+//        }
+//        .fullScreenCover(isPresented: $viewModel.isCaptured) {
+//            if let image = viewModel.image {
+//                CapturedImageView(viewModel: viewModel, image: image)
+//            }
+//        }
+//    }
+//}
+//
+//struct CapturedImageView: View {
+//    @Bindable var viewModel: CameraViewModel
+//    let image: UIImage
+//    @Environment(\.dismiss) private var dismiss
+//    
+//    var body: some View {
+//        VStack {
+//            Image(uiImage: image)
+//                .resizable()
+//                .scaledToFit()
+//                .ignoresSafeArea()
+//                
+//            Button("Use Photo"){
+//                viewModel.retake()
+//                dismiss()
+//            }
+//            .padding()
+//        }
+//    }
+//    
+//
+//
+//}
